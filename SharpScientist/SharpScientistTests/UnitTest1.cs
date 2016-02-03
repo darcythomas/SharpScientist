19eef656ce4eeb06f1a9db8410fe2c88c26a259c
Darcy <github@darcythomas.com>
2016-02-04 12:08:42 +1300

    Added unit test project

---
+++ SharpScientist/SharpScientistTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace SharpScientistTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);//testing works
+        }
+    }
+}
